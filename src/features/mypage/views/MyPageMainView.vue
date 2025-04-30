24a20a7df6286abf7f89fce345d204444bdb0b47
lddocy <jkcy911219@naver.com>
2025-04-30 17:31:13 +0900

    fix: 중복 코드 제거

--- src/features/mypage/views/MyPageMainView.vue
+++ src/features/mypage/views/MyPageMainView.vue
@@ -59,7 +59,6 @@ import UserInfoItem from '../components/UserInfoItem.vue';
   border: 1px solid #ddd;
   border-radius: 10px;
   background-color: white;
-  border-radius: 10px;
   box-shadow: 0 2px 8px rgba(0,0,0,0.05);
   overflow: hidden;
 }
