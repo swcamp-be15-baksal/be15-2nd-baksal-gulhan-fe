45b7ac93fba75880e7cbf24f8b7d09e4f02c6be6
dxmlk <1101sky@korea.ac.kr>
2025-05-02 09:57:37 +0900

    feat: PaginationBar 및 패키지 목록 조회 페이지 수정

--- src/features/package/components/PackageItem.vue
+++ src/features/package/components/PackageItem.vue
@@ -11,19 +11,21 @@ const { packages } = defineProps({
 <template>
     <div class="card">
         <router-link :to="`/packages/${packages.packageId}`">
-            <div>
-                <div>{{ packages.area }}</div>
+            <div class="package-img">
+                <div class="category-label">{{ packages.area }}</div>
                 <img src="https://d152i3f1t56z95.cloudfront.net/test/image.png" />
             </div>
             <div>
-                <div>{{ packages.title }}</div>
-                <div>{{ formatDate(packages.startDate) }} ~ {{ formatDate(packages.endDate) }}</div>
-                <div>
-                    <div>{{ packages.price.toLocaleString() }}원</div>
-                    <div>
-                        <img :src="heartIcon" />
+                <div class="main-info">{{ packages.title }}</div>
+                <div class="main-info">
+                    {{ formatDate(packages.startDate) }} ~ {{ formatDate(packages.endDate) }}
+                </div>
+                <div class="card-bottom">
+                    <div class="price">{{ packages.price.toLocaleString() }}원</div>
+                    <div class="like-review">
+                        <img :src="heartIcon" style="width: 22.5px; height: 20px" />
                         <span style="color: #ff268f">{{ packages.likeCount ?? 0 }}</span>
-                        <span style="color: #adadad">{{ packages.reviewCount ?? 0 }}</span>
+                        <span style="color: #adadad">({{ packages.reviewCount ?? 0 }})</span>
                     </div>
                 </div>
             </div>
@@ -41,11 +43,64 @@ function formatDate(ts) {
 
 <style scoped>
 .card {
-    border: 1px solid #d9d9d9;
-    border-radius: 12px;
+    border: none;
     overflow: hidden;
     display: flex;
-    flex-direction: column;
-    background-color: white;
+    background-color: #fffdf8;
+    max-width: 480px;
+    max-height: 490px;
+}
+
+.card a {
+    color: inherit;
+    text-decoration: none;
+}
+.package-img {
+    position: relative;
+    width: 100%;
+    margin-bottom: 24px;
+}
+
+.package-img img {
+    width: 100%;
+    height: auto%;
+    object-fit: cover;
+    display: block;
+    border-radius: 20px;
+}
+.category-label {
+    position: absolute;
+    top: 14px;
+    left: 16px;
+    border: none;
+    background-color: #3e4042;
+    border-radius: 8px;
+    font-size: 0.9rem;
+    font-weight: 400;
+    color: white;
+    padding: 4px 12px;
+}
+
+.main-info {
+    font-size: 1.6rem;
+    color: black;
+    font-weight: 700;
+}
+
+.card-bottom {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.price {
+    font-size: 1.6rem;
+    color: black;
+}
+
+.like-review {
+    display: flex;
+    font-size: 1.4rem;
+    align-items: center;
+    gap: 4px;
 }
 </style>
--- src/features/package/components/PackageList.vue
+++ src/features/package/components/PackageList.vue
@@ -2,9 +2,15 @@
 import { ref, computed } from 'vue';
 import PackageItem from '@/features/package/components/PackageItem.vue';
 import packages from '@/features/package/mock/packages.json';
+import PaginationBar from '@/components/common/PaginationBar.vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const currentPage = ref(Number(route.query.page) || 1);
 
 const itemsPerPage = 20;
-const currentPage = ref(1);
 
 const totalPages = computed(() => Math.ceil(packages.length / itemsPerPage));
 
@@ -13,9 +19,13 @@ const paginatedItems = computed(() => {
     return packages.slice(start, start + itemsPerPage);
 });
 
-function goToPage(page) {
+function updatePage(page) {
+    router.push({
+        query: { ...route.query, page: page },
+    });
     currentPage.value = page;
 }
+
 // 여기 pagination 로직 나중에 싹 다 수정해야 됨
 </script>
 
@@ -25,50 +35,22 @@ function goToPage(page) {
             <PackageItem v-for="item in paginatedItems" :key="item.packageId" :packages="item" />
         </div>
 
-        <div class="pagination">
-            <button
-                v-for="page in totalPages"
-                :key="page"
-                @click="goToPage(page)"
-                :class="{ active: currentPage === page }">
-                {{ page }}
-            </button>
-        </div>
+        <PaginationBar
+            :current-page="currentPage"
+            :total-pages="totalPages"
+            @update:page="updatePage" />
     </div>
 </template>
 
 <style scoped>
 .package-list-wrapper {
-    max-width: 960px;
+    max-width: 1040px;
     margin: 0 auto;
-    padding: 40px 20px;
 }
 
 .grid {
     display: grid;
-    grid-template-columns: repeat(2, 1fr); /* 2열 */
-    gap: 32px;
-}
-
-.pagination {
-    margin-top: 40px;
-    display: flex;
-    justify-content: center;
-    gap: 8px;
-}
-
-.pagination button {
-    padding: 6px 12px;
-    border: none;
-    background: #eee;
-    cursor: pointer;
-    border-radius: 4px;
-    font-weight: 500;
-}
-
-.pagination button.active {
-    background: #ff268f;
-    color: white;
-    font-weight: 700;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 52px;
 }
 </style>
