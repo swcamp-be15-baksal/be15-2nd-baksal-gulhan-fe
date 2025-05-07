732067ea3886fe9385cedb4126181233619fe017
ckaudgh <audgh3260@naver.com>
2025-05-08 05:21:31 +0900

    임시 저장

--- src/features/place/api.js
+++ src/features/place/api.js
@@ -1,4 +1,5 @@
 import {api} from '@/plugins/axios';
+import { useAuthStore } from '@/stores/auth.js';
 
 export function getParentArea(){
   return api.get("/areas/list")
@@ -22,4 +23,20 @@ export const checkLike = (params) => api.get('/like/check', { params });
 export const getLike = (params) => api.get('/like/likes', { params });
 
 // 좋아요 토글
-export const toggleLike = (data) => api.post('/like/toggle', data);
+export async function toggleLike(targetId, targetType) {
+  const authStore = useAuthStore();
+  const token = authStore.accessToken;
+
+  if (!token) throw new Error('로그인이 필요합니다.');
+
+  const payload = { targetId, targetType };
+
+  console.log('[좋아요 요청]', payload);
+
+  return api.post('/like/toggle', payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    withCredentials: true,
+  });
+}
